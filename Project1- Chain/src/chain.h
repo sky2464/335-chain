660081cf16e17ed7c73b642807e8300d1548e2b6
Navid <31672446+sky2464@users.noreply.github.com>
2018-09-11 13:08:03 -0400

    copy constructor move constructor

--- Project1- Chain/src/chain.h
+++ Project1- Chain/src/chain.h
@@ -13,85 +13,98 @@ namespace teaching_project {
 // and its sample usage.
 template<typename Object>
 class Chain {
- public:
-  // Default "big five" -- you have to alter them for your assignment.
-  // That means that you will remove the "= default" statement.
-  //  and you will provide an implementation.
+public:
+	// Default "big five" -- you have to alter them for your assignment.
+	// That means that you will remove the "= default" statement.
+	//  and you will provide an implementation.
 
-  // Zero-parameter constructor.
-  Chain() = default;
+	// Zero-parameter constructor.
+	// Chain() = default;
+	Chain() {
+	}
+	;
 
-  // Copy-constructor.
-  Chain(const Chain &rhs) = default;
+	// Copy-constructor.
+	//Chain(const Chain &rhs) = default;
 
-  // Copy-assignment. If you have already written
-  // the copy-constructor and the move-constructor
-  // you can just use:
-  // {
-  // Chain copy = rhs; 
-  // std::swap(*this, copy);
-  // return *this;
-  // }
-  Chain& operator=(const Chain &rhs) = default;
+	Chain(const Chain &rhs) {
+		array_=new Object(*rhs.array);
+	}
 
-  // Move-constructor. 
-  Chain(Chain &&rhs) = default;
+	//Move-constructor
 
-  // Move-assignment.
-  // Just use std::swap() for all variables.
-  Chain& operator=(Chain &&rhs) = default;
+Chain (Chain &&rhs):size_{rhs.size_},array_{rhs.array_}{
+	rhs.array_=nullptr;
+	rhs.size=0;
+}
+	// Copy-assignment. If you have already written
+	// the copy-constructor and the move-constructor
+	// you can just use:
+	// {
+	// Chain copy = rhs;
+	// std::swap(*this, copy);
+	// return *this;
+	// }
+	Chain& operator=(const Chain &rhs) = default;
 
-  ~Chain() {
-    // Provide destructor.
-  }
+	// Move-constructor.
+	Chain(Chain &&rhs) = default;
 
-  // End of big-five.
+	// Move-assignment.
+	// Just use std::swap() for all variables.
+	Chain& operator=(Chain &&rhs) = default;
 
-  // One parameter constructor.
-  Chain(const Object& item) {
-    // Write something.
-  }
+	~Chain() {
+		// Provide destructor.
+	}
 
-  // Read a chain from standard input.
-  void ReadChain() {
-    // Write something.
-  }
+	// End of big-five.
 
-  size_t size() const { // Write something
-  }
+	// One parameter constructor.
+	Chain(const Object& item) {
+		// Write something.
+	}
 
-  // @location: an index to a location in the chain.
-  // @returns the Object at @location.
-  // const version.
-  // abort() if out-of-range.
-  const Object& operator[](size_t location) const { 
-    // Write something
-  }
+	// Read a chain from standard input.
+	void ReadChain() {
+		// Write something.
+	}
 
- // @location: an index to a location in the range.
- // @returns the Object at @location.
- // non-const version.
- // abort() if out-of-range.
- Object& operator[](size_t location) { 
-   // Write something (will be the same as above)
- }
+	size_t size() const { // Write something
+	}
 
- //  @c1: A chain.
- //  @c2: A second chain.
- //  @return the concatenation of the two chains.
- friend Chain operator+(const Chain &c1, const Chain &c2) {
-  // Write something.
- }
+	// @location: an index to a location in the chain.
+	// @returns the Object at @location.
+	// const version.
+	// abort() if out-of-range.
+	const Object& operator[](size_t location) const {
+		// Write something
+	}
 
- // Overloading the << operator.
- friend std::ostream &operator<<(std::ostream &out, const Chain &a_chain) {
-   // Print the chain.
-   return out;
- }
- 
- private:
-  size_t size_;
-  Object *array_;
+	// @location: an index to a location in the range.
+	// @returns the Object at @location.
+	// non-const version.
+	// abort() if out-of-range.
+	Object& operator[](size_t location) {
+		// Write something (will be the same as above)
+	}
+
+	//  @c1: A chain.
+	//  @c2: A second chain.
+	//  @return the concatenation of the two chains.
+	friend Chain operator+(const Chain &c1, const Chain &c2) {
+		// Write something.
+	}
+
+	// Overloading the << operator.
+	friend std::ostream &operator<<(std::ostream &out, const Chain &a_chain) {
+		// Print the chain.
+		return out;
+	}
+
+private:
+	size_t size_;
+	Object *array_;
 };
 
 }  // namespace teaching_project
